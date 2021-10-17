1ef76e86b9c53418e7a2c2c04bdc74dcd3b0cea1
XXXStasnislavXXX <stasperfiliyev@gmail.com>
2021-10-18 01:45:31 +0600

    Урок 4, практическое задание 2-3 парсер + выводит дату и курс

--- Perfiliyev_Stanislav_dz_less_4/Perfiliyev_Stanislav_4_2.py
+++ Perfiliyev_Stanislav_dz_less_4/Perfiliyev_Stanislav_4_2.py
@@ -1,23 +1,21 @@
-from cbr_parser import extract_data  # берем интересующую функцию из парсера
-from cbr_parser import send_request
-
-
-print(send_request())
+from cbr_parser import extract_data, extract_date  # берем интересующую функцию из парсера
 
 
 def currency_rates(currency_code: str):
     currency_code = currency_code.upper()
     data = {}
-    ch_code = extract_data("CharCode")
-    value = extract_data("Value")
-    zip_result = zip(ch_code, value)
-    for name, value in zip_result:
-        data.setdefault(name, value)
+    char_code = extract_data("CharCode")
+    cur_value = extract_data("Value")
+    zip_result = zip(char_code, cur_value)
+    for name, cur_value in zip_result:
+        data.setdefault(name, cur_value)
     if currency_code in data:
-        new_data = str(data[currency_code]).replace(",", ".")
-        return round(float(new_data), 2)
+        new_data = round(float(str(data[currency_code]).replace(",", ".")), 2)
+        return new_data, extract_date()
     else:
-        return None
+        return None, extract_date()
 
 
-print(currency_rates(input("введите код валюты :")))
+val_code = input("введите международный код валюты :")
+cur_cost, cur_date = (currency_rates(val_code))
+print(cur_cost, cur_date, sep=', ')
